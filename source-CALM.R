b29b6ca974f10527e099bd014795815e08932957
cwendorf <45096238+cwendorf@users.noreply.github.com>
2021-08-18 04:48:13 -0500

    Update source-CALM.R

--- source-CALM.R
+++ source-CALM.R
@@ -1,3 +1,20 @@
-# Contrast Algorithms for Linear Models
+# Name: source.github.R
+# Purpose: Source all functions from a GitHub repo
+# Author: Craig Wendorf
+# Date: 2021-8-17
 
-## Source All Available Functions
+source.github <- function(username,repo,branch) {
+  url <- paste("https://github.com/", username, "/", repo, "/archive/refs/heads/", branch, ".zip", sep = "")
+  name <- paste(repo, "-", branch, sep="")
+  file <- paste(name,  ".zip", sep = "")
+  download.file(url=url,destfile=file)
+  unzip(zipfile=file)
+  folder <- paste(name, "/R/", sep="")
+  paths <- list.files(path=folder,full.names=TRUE)
+  for (i in 1:length(paths)) source(paths[i])
+  unlink(file)
+  unlink(name, recursive = TRUE)
+}
+
+# Call for this Repo
+source.github("cwendorf","CALM","main")

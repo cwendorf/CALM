f733ff9edd50658d1b8900ff8074d3e3f06a0eca
cwendorf <45096238+cwendorf@users.noreply.github.com>
2021-08-20 07:22:27 -0500

    Update calm.x.R

--- R/calm.x.R
+++ R/calm.x.R
@@ -27,23 +27,19 @@
   paste0(substring(z, 0, m-n), x)
 }
 
-mean_contrasts <- function(M) {
-  Mi <- solve(if(nrow(M) == ncol(M)) M else cbind(Ave = 1, M))
-  colnames(Mi) <- paste0("m", .zf(1:ncol(Mi)))
-}
 
 ### Conversion Functions
 
-calm.encode <- function(L) {
+calm.encode <- function(L,digits=2) {
   X <- t(L) %*% solve(L %*% t(L))
-  rownames(X)=rownames(X,do.NULL=FALSE,prefix="GROUP:")
-  colnames(X)=colnames(X,do.NULL=FALSE,prefix="CODE:")
-  return(round(X,3))
+  rownames(X)=rownames(X,do.NULL=FALSE,prefix="GROUP ")
+  colnames(X)=colnames(X,do.NULL=FALSE,prefix="CODE ")  
+  return(round(X,digits))
 }
 
-calm.decode <- function(X) {
+calm.decode <- function(X,digits=2) {
   L <- solve(t(X) %*% X) %*% t(X)
-  rownames(L)=rownames(L,do.NULL=FALSE,prefix="HYPOTHESIS:")
-  colnames(L)=colnames(L,do.NULL=FALSE,prefix="GROUP:")    
-  return(round(L,3))
+  rownames(L)=rownames(L,do.NULL=FALSE,prefix="HYPOTH ")
+  colnames(L)=colnames(L,do.NULL=FALSE,prefix="GROUP ")   
+  return(round(L,digits))
 }

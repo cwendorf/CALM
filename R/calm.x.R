f7f9c88628d33c6ee3952701e4281f0783201487
cwendorf <45096238+cwendorf@users.noreply.github.com>
2021-10-01 06:08:58 -0500

    Update calm.x.R

--- R/calm.x.R
+++ R/calm.x.R
@@ -1,14 +1,14 @@
 # Contrast Algorithms for Linear Models
 ## Conversion Functions
 
-calm.encode <- function(L,digits=3) {
+calm.encode <- function(L,digits=4) {
   X <- t(L) %*% solve(L %*% t(L))
   rownames(X)=rownames(X,do.NULL=FALSE,prefix="GROUP ")
   colnames(X)=colnames(X,do.NULL=FALSE,prefix="CODE ")
   return(round(X,digits))
 }
 
-calm.decode <- function(X,digits=3) {
+calm.decode <- function(X,digits=4) {
   L <- solve(t(X) %*% X) %*% t(X)
   rownames(L)=rownames(L,do.NULL=FALSE,prefix="HYPOTH ")
   colnames(L)=colnames(L,do.NULL=FALSE,prefix="GROUP ")   
